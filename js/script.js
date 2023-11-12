214caf2dcfba7e17a58795182642289f090c2c00
1ntrspct <150153766+1ntrspct@users.noreply.github.com>
2023-11-12 18:28:40 +0300

    Some changes

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-const expandButton = document.getElementById('expand-button');
-const arrow = expandButton.querySelector('.arrow');
-const container = document.querySelector('.container');
-const rectangleRight = document.querySelector('.rectangle-right');
-const rectangleButton = document.querySelector('.rectangle-button');
-
-let expanded = false;
-
-expandButton.addEventListener('click', () => {
-  expanded = !expanded;
-  arrow.classList.toggle('rotate');
-  container.classList.toggle('move-left');
-  rectangleRight.classList.toggle('show');
-  rectangleButton.classList.toggle('move-right');
-});
-
 // Initialising the canvas
 var canvas = document.querySelector("canvas"),
   ctx = canvas.getContext("2d");
